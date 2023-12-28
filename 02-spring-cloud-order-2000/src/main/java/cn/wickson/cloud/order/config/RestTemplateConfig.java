9e47f37d3960209bb51977dbb7931375ee2b8f4d
DESKTOP-KRH17BU\minuo-java02 <wickson_zhang@163.com>
2023-12-28 10:06:11 +0800

    优化 RestTemplateConfig

--- 02-spring-cloud-order-2000/src/main/java/cn/wickson/cloud/order/config/RestTemplateConfig.java
+++ 02-spring-cloud-order-2000/src/main/java/cn/wickson/cloud/order/config/RestTemplateConfig.java
@@ -2,6 +2,8 @@ package cn.wickson.cloud.order.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -12,8 +14,15 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
     }
 
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(15000);
+        return factory;
+    }
 }
